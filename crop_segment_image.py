03278452ebe3ce44e7303a35ee53ecfb9e72e7f6
Diar Karim <diarkarim@gmail.com>
2024-02-12 21:55:28 +0000

    vide to images and new fbx added

--- crop_segment_image.py
+++ crop_segment_image.py
@@ -3,11 +3,15 @@ from PIL import Image
 import os 
 from rembg import remove 
 
-path = "RawImages/"
-path2save = "CroppedImages/"
+path = "RawImages/Stuart Take 2 Raw/"
+path2save = "CroppedImages/Stuart Take 2 Cropped and Segmented/"
 
 files = os.listdir(path)
 
+# For testing only look at 5 files 
+# files = files[0:5]
+
+counter = 0
 for file in files: 
 
     # Opens a image in RGB mode
@@ -15,9 +19,9 @@ for file in files:
 
     # Setting the points for cropped image
     left = 0
-    top = 500
+    top = 0
     right = 960
-    bottom = 1750
+    bottom = 1350
     
     # Cropped image of above dimension
     # (It will not change original image)
@@ -27,5 +31,8 @@ for file in files:
     output = remove(im1) 
 
     output.save(path2save + "cs_" + file)
-    
+
+    counter = counter + 1
+    print(str(counter) + " / " + str(len(files)))
+
 im1.show()
